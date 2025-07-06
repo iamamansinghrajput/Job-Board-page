bf6f89c4ced4cd1b4c3e84e4d84bfe1054c95659
Rupsa <iamrupsasarkar@gmail.com>
2025-07-06 23:10:09 +0530

    run

--- src/components/Myapplication.jsx/Applicationbox.jsx
+++ src/components/Myapplication.jsx/Applicationbox.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const Applicationbox = () => {
+const Applicationbox = ({data}) => {
+  const {title,date,status} = data;
   return (
      <div className="bg-white border border-gray-200 rounded-lg p-5 flex justify-between items-start shadow-sm max-w-6xl mx-auto">
       <div>
-        <h3 className="text-lg font-semibold text-gray-900">Frontend Developer</h3>
+        <h3 className="text-lg font-semibold text-gray-900">{title}</h3>
         <div className="flex items-center text-sm text-gray-500 mt-2">
           <span className="mr-2">📅</span>
-          <span>Applied on July 2, 2025</span>
+          <span>Applied on {date}</span>
         </div>
       </div>
       
       <span className="bg-blue-100 text-blue-700 text-sm font-medium px-3 py-1 rounded-full">
-        Submitted
+        {status}
       </span>
     </div>
   )
